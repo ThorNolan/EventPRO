9c74e29cbffeb8cc7615dc7d52420911c4884e0d
Jennifer Powell <jerpowel321@gmail.com>
2019-04-09 16:23:02 -0700

    update task & event controllers, update routes, update event listeners

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -2,13 +2,51 @@ var db = require("../models");
 
 module.exports = {
     create: function(req, res){
-        db.Task
+        db.Tasks
         .create(req.body)
         .then(function(result){
             res.json(result)
         })
-        .catch(function(err) {console.log(err)}
-        )
+        .catch(function(err) {
+            console.log(err)
+        })
     },
-
+    viewComplete: function(req, res){
+        db.Tasks
+        .findAll({
+            where: {
+                taskStatus: 1
+            }
+        })
+        .then(function(result){
+            res.json(result)
+        })
+        .catch(function(err) {
+            console.log(err)
+        })
+    },
+    viewOpen: function(req, res){
+        db.Tasks
+        .findAll({
+            where: {
+                taskStatus: 0
+            }
+        })
+        .then(function(result){
+            res.json(result)
+        })
+        .catch(function(req, res) {
+            console.log(err)
+        })
+    },
+    deleteTask: function(req, res){
+        db.Task
+        .destroy({
+            where: {
+              id: req.params.id
+            }
+          }).then(function(dbTask) {
+            res.json(dbTask);
+          });
+    }
 }
